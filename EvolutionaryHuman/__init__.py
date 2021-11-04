b3beebdad659cd559fbf6121cd79a6de6d08cf31
AlbertUnruh <keenan.noack@gmail.com>
2021-11-04 19:20:08 +0100

    add __init__.py

---
+++ EvolutionaryHuman/__init__.py
@@ -0,0 +1,9 @@
+__author__ = "AlbertUnruh"
+__url__ = "https://github.com/AlbertUnruh/EvolutionaryHuman"
+__license__ = "GNU AGPLv3"
+__copyright__ = f"(c) {__author__}"
+
+
+from .models import *
+
+from .utils import *
